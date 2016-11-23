74bfcaa5e246cf588d83e789bc8028b8ed9600ba
GM Softwares <gmsoftwaree@outlook.com>
2016-11-23 12:34:32 -0200

    v1.0
    Commit Inicial do Projeto

---
+++ app/src/main/java/br/com/gmsoftwares/fenixgymtools/CatCostasActivity.java
@@ -0,0 +1,13 @@
+package br.com.gmsoftwares.fenixgymtools;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CatCostasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat_costas);
+    }
+}
---
+++ app/src/main/java/br/com/gmsoftwares/fenixgymtools/CatPeitoActivity.java
@@ -0,0 +1,13 @@
+package br.com.gmsoftwares.fenixgymtools;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CatPeitoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat_peito);
+    }
+}
