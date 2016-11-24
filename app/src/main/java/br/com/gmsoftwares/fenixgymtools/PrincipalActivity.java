56a8d6eeb1bd419ebc798b5ec5a78bc687ea167d
Matheus Buarque <matheusluiz703@hotmail.com>
2016-11-24 00:47:08 -0200

    v1.0
    Adicionados: ligações entre as activitys

--- app/src/main/java/br/com/gmsoftwares/fenixgymtools/PrincipalActivity.java
+++ app/src/main/java/br/com/gmsoftwares/fenixgymtools/PrincipalActivity.java
@@ -8,26 +8,48 @@ import android.widget.*;
 
 public class PrincipalActivity extends AppCompatActivity {
 
-    private Button btnBiceps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-
-        btnBiceps = (Button) findViewById(R.id.btnBiceps);
-        btnBiceps.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-
-                Intent irBiceps = new Intent(PrincipalActivity.this, CatBicepsActivity.class);
-                PrincipalActivity.this.startActivity(irBiceps);
-                PrincipalActivity.this.finish();
-        }
-        });
-        }
-
     }
+
+    public void abrirBiceps(View view)
+    {
+        Intent it = new Intent(this, CatBicepsActivity.class);
+        startActivity(it);
+    }
+
+    public void abrirCostas(View view)
+    {
+        Intent it = new Intent(this, CatCostasActivity.class);
+        startActivity(it);
+    }
+
+    public void abrirOmbro(View view)
+    {
+        Intent it = new Intent(this, CatOmbroActivity.class);
+        startActivity(it);
+    }
+
+    public void abrirPeito(View view)
+    {
+        Intent it = new Intent(this, CatPeitoActivity.class);
+        startActivity(it);
+    }
+
+    public void abrirPernas(View view)
+    {
+        Intent it = new Intent(this, CatPernasActivity.class);
+        startActivity(it);
+    }
+
+    public void abrirTriceps(View view)
+    {
+        Intent it = new Intent(this, CatTricepsActivity.class);
+        startActivity(it);
+    }
+}
 
 
